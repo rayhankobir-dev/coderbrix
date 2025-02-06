3b1af72965b40398dec9e8c16a313fbfb142e450
rayhankobir-dev <rayhankobir893@gmail.com>
2025-02-07 00:11:07 +0600

    fix: write process content

--- src/components/shared/working-procedure.tsx
+++ src/components/shared/working-procedure.tsx
@@ -1,7 +1,7 @@
-/* eslint-disable @next/next/no-img-element */
 import { JSX } from "react";
+import Image from "next/image";
+import { Check } from "lucide-react";
 import { ReactLenis } from "lenis/react";
-import { Check, ShieldCheck } from "lucide-react";
 
 export default function WorkingProcedure(): JSX.Element {
   return (
@@ -9,14 +9,14 @@ export default function WorkingProcedure(): JSX.Element {
       <section className="max-w-6xl mx-auto px-6 md:px-4">
         <h1 className="max-w-2xl mx-auto text-center py-24 font-medium text-4xl md:text-5xl lg:text-6xl">
           <span className="font-title">Our working process</span> <br />
-          <span className="font-uncut tracking-tighter">Fast convert</span>
+          <span className="font-uncut tracking-tighter">and insights.</span>
         </h1>
 
         <div className="grid md:grid-cols-2 items-start gap-10 md:gap-12 mb-20">
           <div className="lg:sticky top-5 min-h-screen flex flex-col justify-center gap-4 pr-12">
             <div className="space-y-4">
               <h2 className="font-medium font-uncut tracking-tight text-3xl">
-                The large scale benefits of Primex
+                Coderbrix is ready to help you achieve your business goals.
               </h2>
               <p className="text-lg">
                 Our solutions have helped thousands of businesses to reach new
@@ -43,89 +43,130 @@ export default function WorkingProcedure(): JSX.Element {
                 </div>
                 <div className="space-y-2">
                   <h3 className="font-medium font-uncut tracking-tighter text-xl">
-                    That Empowers Job Opportunities for the Local Citizens
+                    Delivering Long-Term Value & Customer Success
                   </h3>
                 </div>
               </li>
 
               <li className="flex items-start gap-4">
-                <ShieldCheck className="h-8 w-8 text-primary" />
-                <span className="space-y-2">
+                <div className="h-8 w-8 bg-primary/10 rounded-full flex items-center justify-center">
+                  <Check className="text-primary h-5 w-5" />
+                </div>
+                <div className="space-y-2">
                   <h3 className="font-medium font-uncut tracking-tighter text-xl">
-                    Increased conversion rate
+                    Leveraging Innovation for Competitive Advantage
                   </h3>
-                  <p className="text-base">
-                    Increased conversion rate by focusing on leads that have.
-                  </p>
-                </span>
+                </div>
               </li>
 
               <li className="flex items-start gap-4">
-                <ShieldCheck className="h-8 w-8 text-primary" />
-                <span className="space-y-2">
+                <div className="h-8 w-8 bg-primary/10 rounded-full flex items-center justify-center">
+                  <Check className="text-primary h-5 w-5" />
+                </div>
+                <div className="space-y-2">
                   <h3 className="font-medium font-uncut tracking-tighter text-xl">
-                    Increased conversion rate
+                    Fostering Collaborative & Transparent Partnerships
                   </h3>
-                  <p className="text-base">
-                    Increased conversion rate by focusing on leads that have.
-                  </p>
-                </span>
+                </div>
+              </li>
+
+              <li className="flex items-start gap-4">
+                <div className="h-8 w-8 bg-primary/10 rounded-full flex items-center justify-center">
+                  <Check className="text-primary h-5 w-5" />
+                </div>
+                <div className="space-y-2">
+                  <h3 className="font-medium font-uncut tracking-tighter text-xl">
+                    Building Scalable Solutions for Global Reach
+                  </h3>
+                </div>
+              </li>
+
+              <li className="flex items-start gap-4">
+                <div className="h-8 w-8 bg-primary/10 rounded-full flex items-center justify-center">
+                  <Check className="text-primary h-5 w-5" />
+                </div>
+                <div className="space-y-2">
+                  <h3 className="font-medium font-uncut tracking-tighter text-xl">
+                    Cost-Effective Solutions with High ROI
+                  </h3>
+                </div>
+              </li>
+
+              <li className="flex items-start gap-4">
+                <div className="h-8 w-8 bg-primary/10 rounded-full flex items-center justify-center">
+                  <Check className="text-primary h-5 w-5" />
+                </div>
+                <div className="space-y-2">
+                  <h3 className="font-medium font-uncut tracking-tighter text-xl">
+                    Agile Development for Faster Delivery
+                  </h3>
+                </div>
               </li>
             </ul>
           </div>
 
           <div className="grid gap-2">
             <div className="relative rounded-xl overflow-hidden">
-              <img
+              <Image
                 className="absolute top-0 left-0 w-full h-full"
                 src="/images/bg/green-strip-bg.png"
                 alt="bg"
+                width={1000}
+                height={1000}
               />
-              <div className="relative h-full w-full flex flex-col items-center justify-center gap-10 px-10 py-10 md:py-14 lg:py-20 bg-primary/30">
+              <div className="relative h-full w-full flex flex-col items-start justify-center gap-10 px-10 py-10 md:py-12 lg:py-14 bg-primary/30">
                 <h1 className="font-medium text-2xl md:text-3xl lg:text-5xl text-white text-tracking-tighter">
-                  The large scale benefits of Coderbrix
+                  Pre-Development (Planning & Design)
                 </h1>
-                <p className="text-sm md:text-base lg:text-lg text-white">
-                  Concentrate on leads that have been thoroughly verified and
-                  are ready to be converted into loyal customers, ensuring a
-                  higher success rate.
-                </p>
+
+                <ul className="flex flex-col gap-1 text-sm md:text-base lg:text-lg text-white list-decimal list-inside">
+                  <li>Requirement gathering & analysis.</li>
+                  <li>Market research & competitor study.</li>
+                  <li>UI/UX design & prototyping.</li>
+                  <li>Client feedback & approval.</li>
+                </ul>
               </div>
             </div>
 
             <div className="relative rounded-xl overflow-hidden">
-              <img
+              <Image
                 className="absolute top-0 left-0 w-full h-full scale-125"
                 src="/images/bg/blue-strip-bg.png"
                 alt="bg"
+                width={1000}
+                height={1000}
               />
-              <div className="relative h-full w-full flex flex-col items-center justify-center gap-10 px-10 py-10 md:py-14 lg:py-20">
+              <div className="relative h-full w-full flex flex-col items-start justify-center gap-10 px-10 py-10 md:py-12 lg:py-14">
                 <h1 className="font-medium text-2xl md:text-3xl lg:text-5xl text-white text-tracking-tighter">
-                  The large scale benefits of Coderbrix
+                  Development & Implementation
                 </h1>
-                <p className="text-sm md:text-base lg:text-lg text-white">
-                  Concentrate on leads that have been thoroughly verified and
-                  are ready to be converted into loyal customers, ensuring a
-                  higher success rate.
-                </p>
+                <ul className="flex flex-col gap-1 text-sm md:text-base lg:text-lg text-white list-decimal list-inside">
+                  <li>Backend & frontend development.</li>
+                  <li>API integration & database setup.</li>
+                  <li>Security & performance optimization.</li>
+                  <li>Iterative development & testing.</li>
+                </ul>
               </div>
             </div>
 
             <div className="relative rounded-xl overflow-hidden">
-              <img
+              <Image
                 className="absolute top-0 left-0 w-full h-full"
                 src="/images/bg/purple-strip-bg.png"
                 alt="bg"
+                width={1000}
+                height={1000}
               />
-              <div className="relative h-full w-full flex flex-col items-center justify-center gap-10 px-10 py-10 md:py-14 lg:py-20 bg-purple-800/30">
+              <div className="relative h-full w-full flex flex-col items-start justify-center gap-10 px-10 py-10 md:py-12 lg:py-14 bg-purple-800/30">
                 <h1 className="font-medium text-2xl md:text-3xl lg:text-5xl text-white text-tracking-tighter">
-                  The large scale benefits of Coderbrix
+                  Deployment & Maintenance
                 </h1>
-                <p className="text-sm md:text-base lg:text-lg text-white">
-                  Concentrate on leads that have been thoroughly verified and
-                  are ready to be converted into loyal customers, ensuring a
-                  higher success rate.
-                </p>
+                <ul className="flex flex-col gap-1 text-sm md:text-base lg:text-lg text-white list-decimal list-inside">
+                  <li>Deployment to production.</li>
+                  <li>Final testing & quality assurance.</li>
+                  <li>Ongoing support & updates.</li>
+                  <li>Performance monitoring & scalability improvements.</li>
+                </ul>
               </div>
             </div>
           </div>
